aff4176c9bdbde7628a4e529dbb92b162e1b7265
rishabh <rishabhkhemu@hotmail.com>
2024-12-01 20:20:44 +0530

    day 1 python

---
+++ day_1/day_1.py
@@ -0,0 +1,36 @@
+import sys
+import re
+import math
+import itertools
+import functools
+import collections
+
+
+day = 1
+
+
+def read_input(day) -> str:
+    with open("./input_day_{day}.txt".format(day=day), "r") as f:
+        input_data = f.read()
+    return input_data
+
+
+def parse_input() -> list:
+    input_data = read_input(day).splitlines()
+    left = []
+    right = []
+    for line in input_data:
+        numbers = line.split()
+        left.append(int(numbers[0]))
+        right.append(int(numbers[1]))
+    return left, right
+
+
+left, right = parse_input()
+left.sort()
+right.sort()
+
+print(sum(abs(a - b) for a, b in zip(left, right)))
+
+freq = collections.Counter(right)
+print(sum(a * freq[a] for a in left if a in freq))
